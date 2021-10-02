b4e560c76abb20341551802d43fc9004f2a92283
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-10-02 16:28:45 -0500

    uwu

--- codered/src/App.js
+++ codered/src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+      <h2>Hello sussy bakas</h2>
+      </nav>
       <header className="App-header">
-        <h2>Hello sussy bakas</h2>
+        <h2>I am the Godfather</h2>
       </header>
     </div>
   );
