c9899ab0a0a9c667847b57c07a85510578a18257
victorchkwok <61713215+victorchkwok@users.noreply.github.com>
2021-10-02 18:06:23 -0500

    hi

--- codered/src/App.js
+++ codered/src/App.js
@@ -1,13 +1,18 @@
+<<<<<<< Updated upstream
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import Notif from './Components/Notifications';
 import TipForm from './Components/tipForm';
+=======
+//import logo from './logo.svg';
+>>>>>>> Stashed changes
 import './App.css';
 
 function App() {
   const [currentTip, setCurrentTip] = useState('');
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <nav>
       <h2>Hello sussy bakas</h2>
       </nav>
@@ -19,6 +24,13 @@ function App() {
       </div>
       <div className="tipform">
       <TipForm></TipForm>
+=======
+      <div className="worksafe">
+      <header className="App-header">
+        <p class="p2">MOCHI</p>
+      </header>
+      <p class="p3">This is a paragraph, shown in the Times New Roman font.</p>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
