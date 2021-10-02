16bcec1b973e2cceb1f0c7ce40575a5a786d13f9
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-10-02 16:16:24 -0500

    uwu

---
+++ codered/src/Components/tipForm.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+const tipForm = (props) => {
+    const initialValues = {
+        tipType: '',
+        tipName: '',
+        tip: ''
+    }
+
+    var [info, setInfo] = useState(initialValues);
+
+    useEffect(() => {
+        
+    })
+}
+
+export default tipForm;
