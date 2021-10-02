cce13c462fc8ee652d26ff11983b0cf891e3a841
Chi Ngo <70230102+ctngo2512@users.noreply.github.com>
2021-10-02 17:19:06 -0500

    made 1 input

--- codered/src/App.js
+++ codered/src/App.js
@@ -1,8 +1,11 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import notif from './Components/Notifications';
 import './App.css';
+import TipForm from './tipForm';
 
 function App() {
+  const [currentTip, setCurrentTip] = useState('');
   return (
     <div className="App">
       <nav>
@@ -10,6 +13,7 @@ function App() {
       </nav>
       <header className="App-header">
         <h2>I am the Godfather</h2>
+        <TipForm>{currentTip}</TipForm>
       </header>
       <div>
         <notif/>
