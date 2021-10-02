e27ab331abbf5eff26b3c4d899af60e5dc3557a5
mmsan999 <msanchiher@gmail.com>
2021-10-02 18:29:42 -0500

    notifs addition

--- codered/src/Components/Notifications.js
+++ codered/src/Components/Notifications.js
@@ -2,8 +2,14 @@ import React, {useState, useEffect} from 'react';
 
 const Notif = (props) => {
     
+    Sitting = "Hey! You've probably been sitting for the past 30 minutes";
+    Breaktime = "It's time for a break. You need one.";
+
     return(
-        <div>This is a notification</div>
+        <div>
+            {Sitting},
+            {Breaktime}
+        </div>
     )
 }
 
