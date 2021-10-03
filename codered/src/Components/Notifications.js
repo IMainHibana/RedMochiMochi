2f211b559148fc07260584419176752d06a7a38b
mmsan999 <msanchiher@gmail.com>
2021-10-02 21:05:51 -0500

    Update Notifications.js

--- codered/src/Components/Notifications.js
+++ codered/src/Components/Notifications.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react'
 
 const Notif = (props) => {
     const Sitting = "Hey! You've probably been sitting for the past 30 minutes.";
+    const Standing = "Hey! You've probably been moving around a lot for the past 30 minutes.";
     const Breaktime = "It's time for a break. You need one.";
     const SittingTips = [
         "Get up, stretch your arms out.", 
@@ -13,11 +14,11 @@ const Notif = (props) => {
     ];
 
     return(
-        <div>
+        <alert>
             <h1>{Sitting}</h1>
             <h2>{Breaktime}</h2>
             <h3>{SittingTips[Math.floor(Math.random()*SittingTips.length)]}</h3>
-        </div>
+        </alert>
     )
 }
 
