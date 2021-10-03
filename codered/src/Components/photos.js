258eafc0c7355113585f29c5dccc208f0f34c906
AaronWright22 <70342377+IMainHibana@users.noreply.github.com>
2021-10-02 22:25:51 -0500

    Update photos.js

--- codered/src/Components/photos.js
+++ codered/src/Components/photos.js
@@ -6,7 +6,7 @@ class Photo extends React.Component {
     render() {
         return (
             <div>
-            <h2> Welcome To The Cum Zone </h2>
+            
             <img src = "/images/download.jpg"
             alt = "" / >
             </div>
